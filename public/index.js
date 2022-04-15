56185d7f53c822ebd23afb697ce995a9ed2bb235
adam-bialy <skrzypeknadachu@gmail.com>
2022-04-15 13:52:11 +0200

    update style

--- public/index.js
+++ public/index.js
@@ -6,7 +6,7 @@ var dirList;
 var freqList;
 
 $("button").prop("disabled", true)
-$("#start").prop("disabled", false)
+$("#play").prop("disabled", false)
 
 fetch("/xdirsxlistx").then(function(response) {
   return response.json()
@@ -20,7 +20,7 @@ fetch("/xfreqsxlistx").then(function(response) {
   freqList = data
 })
 
-$("#start").on("click", function(event) {
+$("#play").on("click", function(event) {
   if (!isSound) {
     sound = new Gapless5({
       tracks: dirList,
@@ -32,29 +32,22 @@ $("#start").on("click", function(event) {
     isOn = false
     index = sound.findTrack("sounds/440Hz.wav")
     sound.gotoTrack(index)
+    updateFreqLabel(index)
     $("button").prop("disabled", false)
-    $("#start").prop("disabled", true)
+    $("#play").html("SOUND OFF")
     $("#volrange").prop("disabled", false)
     $("#volrange").prop("value", 2)
     $("#freqrange").prop("disabled", false)
     $("#freqrange").prop("max", dirList.length-1)
     $("#freqrange").prop("value", index)
+  } else {
+    play()
   }
 })
 
-$("#play").on("click", function(event) {
-  if (isOn) {
-    sound.stop()
-    isOn = false
-    // this.innerHTML = "SOUND ON"
-    $("#play").toggleClass("pressed")
-  } else {
-    sound.play()
-    isOn = true
-    // this.innerHTML = "SOUND OFF"
-    $("#play").toggleClass("pressed")
-  }
-})
+// $("#play").on("click", function() {
+//   play()
+// })
 
 $("#volrange").on("change", function(event) {
   sound.setVolume(this.value/10)
@@ -63,24 +56,53 @@ $("#volrange").on("change", function(event) {
 $("#freqrange").on("change", function(event) {
   index = Number(this.value)
   sound.gotoTrack(index)
-  $("h6").html(freqList[index] + " Hz")
+  updateFreqLabel(index)
 })
 
 $(".lower-button").on("click", function(event) {
-  $(".lower").html($("h6").text())
+  // $("#lower").html($("h6").text())
+  $("#lower").val($("h5").text())
+  animate(".lower-button")
 })
 
 $(".upper-button").on("click", function(event) {
-  $(".upper").html($("h6").text())
+  // $("#upper").html($("h6").text())
+  $("#upper").val($("h5").text())
+  animate(".upper-button")
 })
 
 $("#report").on("click", function(event) {
   var url = "https://freq-report.herokuapp.com/?"
   var name = $(".name-input").val()
-  var lower = $(".lower").text().split(" ")[0]
-  var upper = $(".upper").text().split(" ")[0]
+  var lower = $("#lower").val().split(" ")[0]
+  var upper = $("#upper").val().split(" ")[0]
   window.open(
     url + "name=" + name + "&lower=" + lower + "&upper=" + upper,
     "_blank"
   );
 })
+
+function play() {
+  if (isOn) {
+    sound.stop()
+    isOn = false
+    $("#play").html("SOUND OFF")
+    $("#play").toggleClass("pressed")
+  } else {
+    sound.play()
+    isOn = true
+    $("#play").html("SOUND ON")
+    $("#play").toggleClass("pressed")
+  }
+}
+
+function animate(button) {
+  $(button).toggleClass("pressed")
+  setTimeout(function(){
+    $(button).toggleClass("pressed")
+  }, 100)
+}
+
+function updateFreqLabel(index) {
+  $("h5").html(freqList[index] + " Hz")
+}
